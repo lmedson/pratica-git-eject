334e14026e60847a0b4bb6a7cd9d45315e80fb56
Rahquel <rahquel@outlook.com.br>
2019-02-05 19:27:21 -0200

    adiciona conflito

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ server.use('/',(req,res)=>{
  res.send('seu nome aqui.');
 });
 
+server.use('/teste',(req,res)=>{
+ res.send('Hello conflito.');
+});
+
 server.listen(3000,()=>console.log('serve running on port 3000'))
